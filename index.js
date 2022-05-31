cf5d22fc4cc0010ac976709e518b2487f310092c
rafidmahin20 <submanmahin@gmail.com>
2022-05-31 12:38:53 +0600

    edited

--- index.js
+++ index.js
@@ -12,6 +12,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        await client.connect()
+        const serviceCollection = client.db('Jerin-parlour').collection('services');
+
+        app.get('/service', async(req, res) =>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally{}
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) =>{
     res.send('hello from jerin parlour')
 })
